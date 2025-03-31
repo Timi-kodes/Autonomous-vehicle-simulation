e15da4db962954e0ecc20f4d7a83c008612a5f07
Timi <joshuatimi39@gmail.com>
2025-03-31 18:30:48 +0300

    Initial commit

---
+++ install/jetracer_description/share/jetracer_description/launch/display.launch.py
@@ -0,0 +1,38 @@
+from launch import LaunchDescription
+import os
+from ament_index_python.packages import get_package_share_path  
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    urdf_path = os.path.join(get_package_share_path('jetracer_description'), 'urdf', 'jetracer.urdf')
+
+    robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
+
+    return LaunchDescription([
+        # Declare the robot state publisher node
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            parameters=[{'robot_description': robot_description}],      
+        ),
+
+        # Declare the RViz node
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', os.path.join(get_package_share_path('jetracer_description'), 'rviz', 'jetracer.rviz')],
+            output='screen'
+        ),
+
+        # Declare the joint state publisher GUI node
+        Node(
+            package="joint_state_publisher_gui",
+            executable="joint_state_publisher_gui",
+            name="joint_state_publisher_gui",
+        )
+    ])
