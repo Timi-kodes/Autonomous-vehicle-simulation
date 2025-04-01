60e5f1318574beae121a2ea02f8d2d614af0a215
Timi <joshuatimi39@gmail.com>
2025-04-01 21:17:25 +0300

    continue gazebo linking

---
+++ build/jetracer_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/timi/ros2_ws/src/jetracer_bringup
+# Build directory: /home/timi/ros2_ws/build/jetracer_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
